885280d4b2df13a68d195261903755caad42328d
minhsama2000 <minhsama2000@gmail.com>
2021-11-13 15:47:39 +0700

    update login sigin form

---
+++ src/main/java/com/group7/fruitswebsite/controller/client/LoginSiginController.java
@@ -0,0 +1,18 @@
+package com.group7.fruitswebsite.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginSiginController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "client/login";
+	}
+	
+	@GetMapping("/sigin")
+	public String sigin() {
+		return "client/sigin";
+	}
+}
