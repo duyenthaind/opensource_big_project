585f15b9f01b5432be41306442383591a4869213
minhsama2000 <minhsama2000@gmail.com>
2021-11-25 17:15:39 +0700

    update cart

--- src/main/resources/static/client/js/cart.js
+++ src/main/resources/static/client/js/cart.js
@@ -74,6 +74,24 @@ function loadCart(){
                                    +' </td></tr>';
 			}
 			$("#tableCart").append(html);
+			var proQty = $('.pro-qty');
+		    proQty.prepend('<span class="dec qtybtn">-</span>');
+		    proQty.append('<span class="inc qtybtn">+</span>');
+		    proQty.on('click', '.qtybtn', function () {
+		        var $button = $(this);
+		        var oldValue = $button.parent().find('input').val();
+		        if ($button.hasClass('inc')) {
+		            var newVal = parseFloat(oldValue) + 1;
+		        } else {
+		            // Don't allow decrementing below zero
+		            if (oldValue > 0) {
+		                var newVal = parseFloat(oldValue) - 1;
+		            } else {
+		                newVal = 0;
+		            }
+		        }
+		        $button.parent().find('input').val(newVal);
+		    });
 		},
 		error : function(jqXhr, textStatus, errorMessage) { // error
 			// callback
@@ -84,7 +102,8 @@ function loadCart(){
 
 (function () {
 	updateTotal()
-	loadCart()// I will invoke myself
+	loadCart()
+	
 })();
 
 
